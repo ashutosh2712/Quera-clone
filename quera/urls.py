8f245c5e4a6e17b335940b2d7b6b0fc02491d5fd
Ashutosh Kumar Roy <ashu00619@gmail.com>
2023-11-15 01:10:31 +0530

    Authentication revisited

--- quera/urls.py
+++ quera/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("home/", views.home, name="home"),
     path("login/", views.loginPage, name="login"),
+    path("logout/", views.logoutUser, name="logout"),
     path("signup/", views.signup, name="signup"),
     path("postques/", views.postques),
     path("postans/", views.postans),
--- quera/views.py
+++ quera/views.py
@@ -3,41 +3,54 @@ from django.shortcuts import render, redirect
 from .forms import CreateUserForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+@login_required(login_url="login")
 def home(request):
     return render(request, "homepage.html")
 
 
 def signup(request):
-    form = CreateUserForm()
-    if request.method == "POST":
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get("username")
-            messages.success(request, "Account was Created for " + user)
-            return redirect("login")
+    if request.user.is_authenticated:
+        return redirect("home")
+    else:
+        form = CreateUserForm()
+        if request.method == "POST":
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get("username")
+                messages.success(request, "Account was Created for " + user)
+                return redirect("login")
 
-    context = {"form": form}
-    return render(request, "signup.html", context)
+        context = {"form": form}
+        return render(request, "signup.html", context)
 
 
 def loginPage(request):
-    if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = authenticate(request, username=username, password=password)
+    if request.user.is_authenticated:
+        return redirect("home")
+    else:
+        if request.method == "POST":
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+            user = authenticate(request, username=username, password=password)
 
-        if user is not None:
-            login(request, user)
-            return redirect("home")
-        else:
-            messages.error(request, "Username or Password is incorrect")
+            if user is not None:
+                login(request, user)
+                return redirect("home")
+            else:
+                messages.error(request, "Username or Password is incorrect")
 
-    context = {}
-    return render(request, "login.html", context)
+        context = {}
+        return render(request, "login.html", context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect("login")
 
 
 def postques(request):
