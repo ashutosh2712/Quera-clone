2f4b5bfbacbdea9269fadcacd7aaa1a5d5586ea6
Ashutosh Kumar Roy <ashu00619@gmail.com>
2023-12-01 19:57:48 +0530

    Refactor for whitnoise INSTALLED_APP setting

--- QueraClone/settings.py
+++ QueraClone/settings.py
@@ -37,7 +37,8 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    "whitenoise.runserver_nostatic" "django.contrib.admin",
+    "whitenoise.runserver_nostatic",
+    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
